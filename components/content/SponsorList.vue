5ebc8ee2b5d8eabd48a6113930b858413f7bc35c
Jiyao06 <jiyaozhang@stu.pku.edu.cn>
2025-10-23 15:58:34 +0800

    update sponsor

--- components/content/SponsorList.vue
+++ components/content/SponsorList.vue
@@ -16,15 +16,19 @@ const logoHeight = 'h-16 md:h-24'
 
 <template>
   <div class="flex not-prose gap-8 my-8 flex-col">
-    <div v-for="item in avatars" :key="item.name" class="flex flex-col lg:flex-row items-center lg:items-start gap-6 lg:gap-8">
+    <div v-for="item in avatars" :key="item.name" class="flex flex-col items-center gap-6">
       <!-- Logo section -->
       <div class="flex-shrink-0">
         <NuxtImg :src="item.image" alt="Sponsor Logo" :class="['rounded-lg shadow-lg object-contain', logoHeight, logoWidth]" />
       </div>
       
+      <!-- Company name -->
+      <div class="text-center">
+        <a :href="item.link" target="_blank" class="font-semibold text-primary text-xl lg:text-2xl">{{ item.name }}</a>
+      </div>
+      
       <!-- Text description section -->
-      <div class="flex-1 text-center lg:text-left">
-        <a :href="item.link" target="_blank" class="font-semibold text-primary text-xl lg:text-2xl block mb-3">{{ item.name }}</a>
+      <div class="w-full max-w-4xl text-center">
         <div class="text-gray-700 dark:text-gray-300 text-base lg:text-lg leading-relaxed">{{ item.description }}</div>
       </div>
     </div>
